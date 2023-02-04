b720e232641fc4e7b04ce8682fc9489395d0a7e4
Bemnet Nikodimos <44458004+Bemmy7@users.noreply.github.com>
2023-02-04 21:42:49 +0300

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import {Game} from "./components/Game";
+import {MainMenu} from "./components/MainMenu";
+import {StartGame} from "./components/StartGame";
+import {Settings} from "./components/Settings";
+import {NotFound} from "./components/NotFound";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Routes>
+        <Route path="/" element={<MainMenu/>} />
+        <Route path="/game" element={<Game/>} />
+        <Route path="/start" element={<StartGame/>} />
+        <Route path="/settings" element={<Settings/>} />
+        <Route path="*" element={<NotFound/>} />
+       </Routes>
     </div>
   )
 }
