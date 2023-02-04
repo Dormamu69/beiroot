38d1d704664211bc5f5d95022199c442ce246c10
Bemnet Nikodimos <44458004+Bemmy7@users.noreply.github.com>
2023-02-04 23:16:00 +0300

    finished main menu

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
-import './index.css'
+// import './index.css'
+import 'bulma/css/bulma.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,9 +1,73 @@
-import React from 'react';
-
-
+import React from "react";
 
 const MainMenu = () => {
-    return ( <h1>Main Menu</h1> );
-}
- 
+  return (
+    <div style={{ backgroundColor: "#f2f2f2" }}>
+      <div className={"content is-fluid"}>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span ariaHidden="true"></span>
+              <span ariaHidden="true"></span>
+              <span ariaHidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <div className="buttons">
+                <a className="button is-primary">Full Screen Icon</a>
+              </div>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-primary">Mute/Unmute Icon</a>
+                  {/* <a className="button is-primary">Full Screen Icon</a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            {/* using bulma css create a main menu with play,leaderboard,how to play options */}
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-half">
+                  <div className="column is-centered">
+                    <h1 className="title">BaeRoot</h1>
+                    <h4 className="subtitle">Find the root of a word</h4>
+                  </div>
+                  <div className="box">
+                    <div className="columns is-centered">
+                      <div className="buttons is-centered">
+                        <a className="button is-primary is-fullwidth">Play</a>
+                        <a className="button is-primary is-fullwidth">
+                          Leaderboard
+                        </a>
+                        <a className="button is-primary is-fullwidth">
+                          How To Play
+                        </a>
+                        <a className="button is-primary is-fullwidth">Quit</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
 export default MainMenu;
