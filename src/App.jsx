7c0d4c6791385cf8081f28cbd113c42ca1f718d1
Chapi Menge <chapimenge3@gmail.com>
2023-02-04 23:13:08 +0300

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Game from "./pages/Game";
-import MainMenu from "./pages/MainMenu";
-import StartGame from "./pages/StartGame";
-import Settings from "./pages/Settings";
-import NotFound from "./pages/NotFound";
+import  Game from "./pages/Game";
+import  MainMenu from "./pages/MainMenu";
+import  StartGame from "./pages/StartGame";
+import  Settings from "./pages/Settings";
+import  NotFound from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-       <Routes>
-        <Route path="/" element={<MainMenu/>} />
-        <Route path="/game" element={<Game/>} />
-        <Route path="/start" element={<StartGame/>} />
-        <Route path="/settings" element={<Settings/>} />
-        <Route path="*" element={<NotFound/>} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/start" element={<StartGame />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="not-found">
       <h1>404: Page Not Found</h1>
       <p>Sorry, the page you are looking for could not be found.</p>
     </div>
