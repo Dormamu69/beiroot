010a5483a1163f58987745b19e97944fb22245f6
Bemnet Nikodimos <44458004+Bemmy7@users.noreply.github.com>
2023-02-07 23:54:37 +0300

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -7,10 +7,17 @@ import  MainMenu from "./pages/MainMenu";
 import  HowToPlay from "./pages/HowToPlay";
 import  LeaderBoard from "./pages/LeaderBoard";
 import  NotFound from "./pages/NotFound";
+import ReactGA from "react-ga";
 
 function App() {
   const [count, setCount] = useState(0);
+  const TRACKING_ID = "UA-205453516-2"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <Routes>
