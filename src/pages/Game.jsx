5c7e7709fb272928156b6b41f5f5540553515837
--replace-all <--replace-all>
2023-02-10 11:31:43 +0300

    button collor s ?

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -323,7 +323,7 @@ const Game = ({ letters }) => {
           )}
           <div className="buttons is-centered">
             <button
-              className="button is-primary"
+              className="button is-sucess"
               onClick={() => {
                 handleSubmit();
               }}
@@ -340,7 +340,7 @@ const Game = ({ letters }) => {
               />
             )}
             <button
-              className="button is-primary"
+              className="button is-secondary"
               onClick={() => {
                 newWord();
               }}
@@ -358,7 +358,7 @@ const Game = ({ letters }) => {
               እንደገና ይጀምሩ
             </button>
             <button
-              className="button is-primary"
+              className="button is-secondary"
               onClick={() => {
                 setSelectedDifficulty(undefined);
               }}
